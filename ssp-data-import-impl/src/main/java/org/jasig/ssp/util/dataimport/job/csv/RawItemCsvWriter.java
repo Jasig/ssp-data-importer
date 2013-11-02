ab9a6f5012c28c602582427d18e0eb786af55159
Dan McCallum <dan.mccallum@gmail.com>
2013-11-01 19:37:43 -0700

    NOJIRA Fix class cast exceptions

--- ssp-data-import-impl/src/main/java/org/jasig/ssp/util/dataimport/job/csv/RawItemCsvWriter.java
+++ ssp-data-import-impl/src/main/java/org/jasig/ssp/util/dataimport/job/csv/RawItemCsvWriter.java
@@ -1,5 +1,6 @@
 package org.jasig.ssp.util.dataimport.job.csv;
 
+import org.jasig.ssp.util.dataimport.job.domain.RawItem;
 import org.jasig.ssp.util.dataimport.job.domain.RawItemAndBean;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.core.io.Resource;
@@ -8,13 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class RawItemAndBeanCsvWriter implements ItemWriter<RawItemAndBean> {
+public class RawItemCsvWriter implements ItemWriter<RawItem> {
 
     private Resource currentResource;
     private String[] orderedHeaders = null;
 
     @Override
-    public void write(List<? extends RawItemAndBean> items) throws Exception {
+    public void write(List<? extends RawItem> items) throws Exception {
         // TODO Extend FlatFileItemWriter instead. For now we're just writing to std out for prototyping.
         // As written, this only works if the bean is step-scoped. Might be a bit challenging even in a
         // FlatFileItemWriter subclass to write an item-based header row.
@@ -23,15 +24,15 @@ public class RawItemAndBeanCsvWriter implements ItemWriter<RawItemAndBean> {
             orderedHeaders = null;
             return;
         }
-        if ( currentResource == null || !(currentResource.equals(items.get(0).getRawItem().getResource())) ) {
+        if ( currentResource == null || !(currentResource.equals(items.get(0).getResource())) ) {
             if ( currentResource != null ) {
                 say("");
             }
             orderedHeaders = writeHeader(items);
         }
-        for ( RawItemAndBean item : items ) {
+        for ( RawItem item : items ) {
             StringBuilder sb = new StringBuilder();
-            final Map<String,String> record = item.getRawItem().getRecord();
+            final Map<String,String> record = item.getRecord();
             for ( String header : orderedHeaders ) {
                 sb.append(record.get(header)).append(",");
             }
@@ -40,8 +41,8 @@ public class RawItemAndBeanCsvWriter implements ItemWriter<RawItemAndBean> {
         }
     }
 
-    private String[] writeHeader(List<? extends RawItemAndBean> items) {
-        Map<String,String> firstRecord = items.get(0).getRawItem().getRecord();
+    private String[] writeHeader(List<? extends RawItem> items) {
+        Map<String,String> firstRecord = items.get(0).getRecord();
         StringBuilder sb = new StringBuilder();
         List<String> headerColumns = new ArrayList<String>();
         for ( String key : firstRecord.keySet() ) {
--- ssp-data-import-impl/src/main/java/org/jasig/ssp/util/dataimport/job/domain/RawItemAndBean.java
+++ ssp-data-import-impl/src/main/java/org/jasig/ssp/util/dataimport/job/domain/RawItemAndBean.java
@@ -1,10 +1,14 @@
 package org.jasig.ssp.util.dataimport.job.domain;
 
+import org.springframework.core.io.Resource;
+
+import java.util.Map;
+
 /**
  * Holds a {@link RawItem} and an arbitrary object associated with it, typically a SSP entity bean onto which it was
  * mapped.
  */
-public class RawItemAndBean {
+public class RawItemAndBean extends RawItem {
 
     private RawItem rawItem;
     private Object bean;
@@ -15,6 +19,31 @@ public class RawItemAndBean {
 
     public void setRawItem(RawItem rawItem) {
         this.rawItem = rawItem;
+    }
+
+    @Override
+    public void setResource(Resource resource) {
+        if ( rawItem == null ) {
+            rawItem = new RawItem();
+        }
+        rawItem.setResource(resource);
+    }
+
+    @Override
+    public Resource getResource() {
+        return rawItem == null ? null : rawItem.getResource();
+    }
+
+    @Override
+    public void setRecord(Map<String,String> record) {
+        if ( rawItem == null ) {
+            rawItem = new RawItem();
+        }
+        rawItem.setRecord(record);
+    }
+
+    public Map<String,String> getRecord() {
+        return rawItem == null ? null : rawItem.getRecord();
     }
 
     public Object getBean() {
