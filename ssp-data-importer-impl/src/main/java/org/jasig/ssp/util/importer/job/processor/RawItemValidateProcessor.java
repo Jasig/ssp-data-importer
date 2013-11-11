9c8486b6d5a1aa24b376271e32698bb57dcfb70f
Jim Stanley <jstanley@unicon.net>
2013-11-11 11:30:44 -0700

    NOJIRA Validation Runs

--- ssp-data-importer-impl/src/main/java/org/jasig/ssp/util/importer/job/processor/RawItemValidateProcessor.java
+++ ssp-data-importer-impl/src/main/java/org/jasig/ssp/util/importer/job/processor/RawItemValidateProcessor.java
@@ -1,12 +1,10 @@
 package org.jasig.ssp.util.importer.job.processor;
 
 
-import org.hibernate.validator.metadata.ConstraintDescriptorImpl;
 import org.jasig.ssp.util.importer.job.config.MetadataConfigurations;
 import org.jasig.ssp.util.importer.job.domain.RawItem;
 import org.jasig.ssp.util.importer.job.validation.map.metadata.MapConstraintDescriptor;
 import org.jasig.ssp.util.importer.job.validation.map.metadata.utils.MapReference;
-import org.jasig.ssp.util.importer.job.validation.map.metadata.validation.DatabaseConstraintMapValidator;
 import org.jasig.ssp.util.importer.job.validation.map.metadata.validation.MapConstraintValidatorContext;
 import org.springframework.batch.item.ItemProcessor;
 
@@ -20,10 +18,10 @@ public class RawItemValidateProcessor implements ItemProcessor<RawItem,RawItem> 
     public RawItem process(RawItem item) throws Exception {
         item.getRecord();
         String fileName = item.getResource().getFilename();
-        String[] tableNames = fileName.split("\\.");
-        MapReference mapReference = new MapReference(item.getRecord(), tableNames[0], null);
+        String[] tableName = fileName.split("\\.");
+        MapReference mapReference = new MapReference(item.getRecord(), tableName[0], null);
         Boolean isValid = metadataRepository.getRepository().isValid(mapReference,
-                new  MapConstraintValidatorContext( new MapConstraintDescriptor(), null, null));
+                new  MapConstraintValidatorContext( new MapConstraintDescriptor()));
         if(isValid == false){
             throw new Exception("bean not valid");
         }
