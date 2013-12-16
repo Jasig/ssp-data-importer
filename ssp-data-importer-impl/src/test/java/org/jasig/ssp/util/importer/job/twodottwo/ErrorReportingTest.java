a03dd18cc16813a70c8e7149c38ba4e684be9bc0
Jim Stanley <jstanley@unicon.net>
2013-12-16 16:02:41 -0700

    SSP-1951 added error reporting test for previously unreported failures

---
+++ ssp-data-importer-impl/src/test/java/org/jasig/ssp/util/importer/job/twodottwo/ErrorReportingTest.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.ssp.util.importer.job.twodottwo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.jasig.ssp.util.importer.job.report.ErrorEntry;
+import org.jasig.ssp.util.importer.job.report.ReportEntry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/twodottwo-test-error-reporting/launch-context-test.xml")
+public class ErrorReportingTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testJob() throws Exception {
+
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+
+        Map<String, ReportEntry> report = (Map<String, ReportEntry>)jobExecution.getExecutionContext().get("report");
+        Assert.assertNotNull(report);
+        Set<Entry<String, ReportEntry>> entrySet = report.entrySet();
+        Assert.assertEquals(1, entrySet.size());
+        for (Entry<String, ReportEntry> entry : entrySet) {
+            Assert.assertEquals(new Integer(0), entry.getValue().getNumberParsed());
+            Assert.assertEquals(new Integer(0), entry.getValue().getNumberSkippedOnParse());
+            Assert.assertEquals(new Integer(2), entry.getValue().getNumberSkippedOnDatabaseWrite());
+            Assert.assertEquals(new Integer(0), entry.getValue().getNumberInsertedUpdated());
+        }
+        List<ErrorEntry> errors =(List<ErrorEntry>) jobExecution.getExecutionContext().get("errors");
+        Assert.assertEquals(2, errors.size());
+
+    }
+}
